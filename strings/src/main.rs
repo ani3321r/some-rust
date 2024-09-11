e8d90ccd377beedaf58960c118f7ec54aa8515dc
ani3321r <ani3321r@gmail.com>
2024-09-11 14:35:08 +0530

    strings part2

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,4 @@
+use unicode_segmentation::UnicodeSegmentation; //for getting grapheme cluster we need to include this
 fn main() {
     // ways to initialize a string
     let s1 = String::new();
@@ -8,6 +9,7 @@ fn main() {
     // as strings are utf-8 encoded many languages are supported
     let hello = String::from("नमस्ते");
     let hello = String::from("こんにちは");
+    let hello = String::from("😊"); // also has emoji support
 
     //appending
     let mut s = String::from("postgress");
@@ -18,4 +20,28 @@ fn main() {
     let s2 = String::from("raiden");
     // let s3: String = s1 + &s2;
     let s4 = format!("{}{}",s1,s2); //using format macro
+
+    // a char can't be specified from a string just by using sqare braces and index val, because its 
+    // utf-8 encoded
+    let hello: String = String::from("नमस्ते");
+    // in bytes
+    // [224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164, 224, 165, 135]
+
+    for b in "नमस्ते".bytes(){
+        println!("{}", b);
+    } //iterates over the bytes of every char
+
+    //scaler values
+    // ['न', 'म', 'स', '्', 'त', 'े']
+
+    for c in "नमस्ते".chars(){
+        println!("{}", c);
+    }
+
+    // Grapheme Clusters
+    // ["न", "म", "स्", "ते"]
+
+    for g in "नमस्ते".graphemes(true){
+        println!("{}", g);
+    }
 }
