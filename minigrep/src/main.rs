98ef11dbebfc7474fb95109ed77773d05576d5a6
ani3321r <ani3321r@gmail.com>
2024-09-16 22:02:09 +0530

    cli app part1

---
+++ minigrep/src/main.rs
@@ -0,0 +1,49 @@
+use  std::env;
+use std::fs;
+use std::process; //exit the program without panicking
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // the args func will give us an iterator over arguments passed to program
+    // collect will turn it into a collection
+
+    let config = Config::new(&args).unwrap_or_else( |err |{
+        println!("Problem passing args: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config){
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    //reading a file
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("text content:\n{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config{
+    fn new(args: &[String]) -> Result<Config, &str>{// convention for constructor func(calling new)
+        if args.len() < 3{
+            return Err("not enough args");
+        }
+        let query =  args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config {query, filename})
+    }
+}
