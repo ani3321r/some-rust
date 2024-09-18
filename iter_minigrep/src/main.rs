2516643d95418ca2415b7f2fc148f3be2bde5fb8
ani3321r <ani3321r@gmail.com>
2024-09-18 22:39:06 +0530

    refactoring minigrep using iterators

---
+++ iter_minigrep/src/main.rs
@@ -0,0 +1,23 @@
+use  std::env;
+use std::process; //exit the program without panicking
+
+use minigrep::Config;
+
+fn main() {
+
+    let config = Config::new(env::args()).unwrap_or_else( |err |{
+        eprintln!("Problem passing args: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config){
+        eprintln!("Application Error: {}", e); //eprintln for printing error string
+        process::exit(1);
+    }
+}
+
+// to get the standard output in a seperate file we use the following command
+// cargo run to poem.txt > output.txt
