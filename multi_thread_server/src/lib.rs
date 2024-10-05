177918fd99daa02c86297598fce0a4471f52abd3
ani3321r <ani3321r@gmail.com>
2024-10-05 12:05:17 +0530

    shut down and cleanup of the server

--- multi_thread_server/src/lib.rs
+++ multi_thread_server/src/lib.rs
@@ -2,10 +2,15 @@ use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 pub struct ThreadPool{
   workers: Vec<Worker>,
-  sender: mpsc::Sender<Job>,
+  sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message{
+  NewJob(Job),
+  Terminate,
+}
 
 impl ThreadPool{
   pub fn new(size: usize) -> ThreadPool{
@@ -28,29 +33,56 @@ impl ThreadPool{
     F: FnOnce() + Send + 'static
   {
     let job = Box::new(f);
-    self.sender.send(job).unwrap();
+    self.sender.send(Message::NewJob(job)).unwrap();
   }  
+}
+
+impl Drop for ThreadPool{
+  fn drop(&mut self){
+    println!("sending terminate message to all workers");
+    for _ in &self.workers{
+      self.sender.send(Message::Terminate).unwrap();
+    }
+
+    println!("shutting down all workers");
+
+    for worker in &mut self.workers{
+      println!("shutting down worker {}", worker.id);
+
+      if let Some(thread) = worker.thread.take(){
+        thread.join().unwrap();
+      }
+    }
+  }
 }
 
 struct Worker{
   id: usize,
-  thread: thread::JoinHandle<()>
+  thread: Option<thread::JoinHandle<()>>
 }
 
 impl Worker{
-  fn new (id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{
+  fn new (id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker{
     let thread = thread::spawn(move || loop{
-      let job = receiver
+      let message = receiver
         .lock()
         .unwrap()
         .recv()
         .unwrap();
 
-        println!("worker {} got a job executing", id);
-        job();
+        match message{
+          Message::NewJob(job) => {
+            println!("Worker {} got a job executing", id);
+            job();
+          }
+          Message::Terminate => {
+            println!("worker {} was told to terminate", id);
+            break;
+          }
+        }
     });
 
-    Worker{id, thread}
+    Worker{id, thread: Some(thread)}
   }
 }
 
@@ -59,4 +91,21 @@ getting output 😁
 worker 0 got a job executing
 worker 1 got a job executing
 worker 2 got a job executing
+*/
+
+
+// after implimenting shut down and cleanup for the server, the output
+/*
+Worker 0 got a job executing
+sending terminate message to all workers
+shutting down all workers
+shutting down worker 0
+worker 2 was told to terminate
+worker 3 was told to terminate
+Worker 1 got a job executing
+worker 0 was told to terminate
+shutting down worker 1
+worker 1 was told to terminate
+shutting down worker 2
+shutting down worker 3
 */
